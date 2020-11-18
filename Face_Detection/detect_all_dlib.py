fa35d7dc903b498928a50deb08bbe14395c17f56
Abdullah Selek <abdullahselek@gmail.com>
2020-11-18 22:14:02 +0000

    Code formatting on face detection

--- Face_Detection/detect_all_dlib.py
+++ Face_Detection/detect_all_dlib.py
@@ -26,7 +26,11 @@ import dlib
 
 def _standard_face_pts():
     pts = (
-        np.array([196.0, 226.0, 316.0, 226.0, 256.0, 286.0, 220.0, 360.4, 292.0, 360.4], np.float32) / 256.0
+        np.array(
+            [196.0, 226.0, 316.0, 226.0, 256.0, 286.0, 220.0, 360.4, 292.0, 360.4],
+            np.float32,
+        )
+        / 256.0
         - 1.0
     )
 
@@ -34,7 +38,10 @@ def _standard_face_pts():
 
 
 def _origin_face_pts():
-    pts = np.array([196.0, 226.0, 316.0, 226.0, 256.0, 286.0, 220.0, 360.4, 292.0, 360.4], np.float32)
+    pts = np.array(
+        [196.0, 226.0, 316.0, 226.0, 256.0, 286.0, 220.0, 360.4, 292.0, 360.4],
+        np.float32,
+    )
 
     return np.reshape(pts, (5, 2))
 
@@ -103,7 +110,12 @@ def show_detection(image, box, landmark):
     print(box[2] - box[0])
     plt.gca().add_patch(
         Rectangle(
-            (box[1], box[0]), box[2] - box[0], box[3] - box[1], linewidth=1, edgecolor="r", facecolor="none"
+            (box[1], box[0]),
+            box[2] - box[0],
+            box[3] - box[1],
+            linewidth=1,
+            edgecolor="r",
+            facecolor="none",
         )
     )
     plt.scatter(landmark[0][0], landmark[0][1])
@@ -120,19 +132,28 @@ def affine2theta(affine, input_w, input_h, target_w, target_h):
     theta = np.zeros([2, 3])
     theta[0, 0] = param[0, 0] * input_h / target_h
     theta[0, 1] = param[0, 1] * input_w / target_h
-    theta[0, 2] = (2 * param[0, 2] + param[0, 0] * input_h + param[0, 1] * input_w) / target_h - 1
+    theta[0, 2] = (
+        2 * param[0, 2] + param[0, 0] * input_h + param[0, 1] * input_w
+    ) / target_h - 1
     theta[1, 0] = param[1, 0] * input_h / target_w
     theta[1, 1] = param[1, 1] * input_w / target_w
-    theta[1, 2] = (2 * param[1, 2] + param[1, 0] * input_h + param[1, 1] * input_w) / target_w - 1
+    theta[1, 2] = (
+        2 * param[1, 2] + param[1, 0] * input_h + param[1, 1] * input_w
+    ) / target_w - 1
     return theta
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--url", type=str, default="/home/jingliao/ziyuwan/celebrities", help="input")
     parser.add_argument(
-        "--save_url", type=str, default="/home/jingliao/ziyuwan/celebrities_detected_face_reid", help="output"
+        "--url", type=str, default="/home/jingliao/ziyuwan/celebrities", help="input"
+    )
+    parser.add_argument(
+        "--save_url",
+        type=str,
+        default="/home/jingliao/ziyuwan/celebrities_detected_face_reid",
+        help="output",
     )
     opts = parser.parse_args()
 
@@ -172,13 +193,17 @@ if __name__ == "__main__":
                 face_landmarks = landmark_locator(image, current_face)
                 current_fl = search(face_landmarks)
 
-                affine = compute_transformation_matrix(image, current_fl, False, target_face_scale=1.3)
+                affine = compute_transformation_matrix(
+                    image, current_fl, False, target_face_scale=1.3
+                )
                 aligned_face = warp(image, affine, output_shape=(256, 256, 3))
                 img_name = x[:-4] + "_" + str(face_id + 1)
-                io.imsave(os.path.join(save_url, img_name + ".png"), img_as_ubyte(aligned_face))
+                io.imsave(
+                    os.path.join(save_url, img_name + ".png"),
+                    img_as_ubyte(aligned_face),
+                )
 
         count += 1
 
         if count % 1000 == 0:
             print("%d have finished ..." % (count))
-
