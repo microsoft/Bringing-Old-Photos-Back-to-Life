91d04bd826c0d7d77fb0aacfced824f80dfa1f34
Aayush-hub <ayushgarg1951@gmail.com>
2021-03-28 20:26:45 +0530

    Added image filter script

---
+++ Photo_Filter/Black_and_White_Effect.py
@@ -0,0 +1,38 @@
+import os
+import argparse
+from PIL import Image 
+
+def blackndwhite(input_folder,output_folder):
+    """ A program to apply filters on Output Images
+    Parameters:
+    --input_folder (str): Input Image folder location
+    --output_folder (str): Output Image folder location
+    
+    """
+    images = os.listdir(opts.input_folder)
+    for image in images:
+        # opening image from input folder
+        image_file = Image.open(opts.input_folder +'/'+ image)
+        # converting image to black and white
+        image_file = image_file.convert('1')
+        # saving image to output folder
+        image_file.save(opts.output_folder + '/' + 'result.png')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_folder", type=str, default="./output", help="Test images")
+    parser.add_argument(
+        "--output_folder",
+        type=str,
+        default="./output",
+        help="Restored images, please use the absolute path",
+    )
+    opts = parser.parse_args()
+    opts.input_folder = os.path.abspath(opts.input_folder)
+    opts.output_folder = os.path.abspath(opts.output_folder)
+    # checking for output folder
+    if not os.path.exists(opts.output_folder):
+        os.makedirs(opts.output_folder)
+    input_folder = opts.input_folder
+    output_folder = opts.output_folder
+    blackndwhite(input_folder,output_folder)
